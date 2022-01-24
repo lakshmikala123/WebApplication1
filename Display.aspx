c172d9252d49b00ce14ccf04cbc483e1600c92ad
lakshmikala123 <98016553+lakshmikala123@users.noreply.github.com>
2022-01-24 15:26:27 +0530

    Add files via upload

---
+++ Display.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Display.aspx.cs" Inherits="WebApplication1.Display" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <label >Employee Details</label>
+    <asp:GridView  runat="server" ID="Empgv" AutoGenerateColumns ="true"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Display.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace WebApplication1
+{
+    public partial class Display : System.Web.UI.Page
+    {
+        //String Connectionstring=ConfigurationManager.ConnectionStrings["oracleDB"].ConnectionString.ToString();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string oradb = "Data Source=L-158031343;User ID=lakshmi;Password=tiger;";
+            //OracleConnection conn = new OracleConnection(oradb);
+            OracleConnection _connection = new OracleConnection(oradb);
+          //  _connection.ConnectionString = Connectionstring;
+            _connection.Open();
+            //string sql = "select * from EMP";
+            OracleCommand command = new OracleCommand();
+            command.Connection = _connection;
+            command.CommandText = "select * from EMP";
+            command.CommandType = CommandType.Text;
+            //DataTable dt = new DataTable();
+           
+         
+            Empgv.DataSource = command.ExecuteReader();
+            Empgv.DataBind();
+            _connection.Close();
+
+        }
+    }
+}
